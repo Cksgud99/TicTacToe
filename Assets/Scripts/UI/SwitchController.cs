e3c2ac269e1d4d6d2167b4498f7aa1a7ca20c2cb
Cksgud99 <cksgud1126@gmail.com>
2025-02-10 11:17:31 +0900

    스위치 구현 완료

--- Assets/Scripts/UI/SwitchController.cs
+++ Assets/Scripts/UI/SwitchController.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
 
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(AudioSource))]
 public class SwitchController : MonoBehaviour
 {
     [SerializeField] private Image handleImage;
+    [SerializeField] private AudioClip clickSound;
     
+    public delegate void OnSwitchChangeDelegate(bool isOn);
+    public OnSwitchChangeDelegate OnSwitchChange;
+    
+    private Image _backgroundImage;
     private RectTransform _handleRectTransform;
+    private AudioSource _audioSource;
+    
     private bool _isOn;
+    
+    private static readonly Color32 OffColor = new Color32(170, 170, 170, 255);
+    private static readonly Color32 OnColor = new Color32(90,90, 90, 255);
 
     private void Awake()
     {
+        _backgroundImage = GetComponent<Image>();
         _handleRectTransform = handleImage.GetComponent<RectTransform>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -26,17 +40,26 @@ public class SwitchController : MonoBehaviour
         if (isOn)
         {
             _handleRectTransform.DOAnchorPosX(14, 0.2f);
+            _backgroundImage.DOBlendableColor(OnColor, 0.2f);
         }
         else
         {
-            _handleRectTransform.DOAnchorPosX(-14, 0.2f); 
+            _handleRectTransform.DOAnchorPosX(-14, 0.2f);
+            _backgroundImage.DOBlendableColor(OffColor, 0.2f);
         }
         
+        OnSwitchChange?.Invoke(isOn);
         _isOn = isOn;
     }
+    
+    
 
     public void OnClickSwitch()
     {
+        // 효과음 재생
+        if (clickSound != null) _audioSource.PlayOneShot(clickSound);
+        
         SetOn(!_isOn);
+        
     }
 }
