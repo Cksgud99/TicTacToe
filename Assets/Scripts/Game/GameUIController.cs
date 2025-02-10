59c2fff1db5db76fc44fbc48c9efa9a4d95be0a3
Cksgud99 <cksgud1126@gmail.com>
2025-02-10 11:45:12 +0900

    Navigation Controller 구현

--- Assets/Scripts/Game/GameUIController.cs
+++ Assets/Scripts/Game/GameUIController.cs
@@ -69,5 +69,18 @@ public class GameUIController : MonoBehaviour
         {
             GameManager.Instance.ChangeToMainScene();
         });
-}
+    }
+
+    public void OnClickSettingsButton()
+    {
+        GameManager.Instance.OpenSettingsPanel();
+    }
+
+    public void OnClickBackButton()
+    {
+        GameManager.Instance.OpenConfirmPanel("게임을 종료하시겠습니까?", () =>
+        {
+            GameManager.Instance.ChangeToMainScene();
+        });
+    }
 }
